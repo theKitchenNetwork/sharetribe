ddccb2229330e5537f749e1ed3a782e7bcce5b3d
RMcNeely -- Ubuntu Desktop <rmcneely23@gmail.com>
2017-08-01 01:12:24 -0400

    Add seeds for development database, update structure.sql to latest. Now able to serve basic marketplace

---
+++ db/seeds.rb
@@ -0,0 +1,18 @@
+### Create custom marketplace
+community = Community.create(ident: "theKitchenNetwork", domain: "rentalkitchennetwork", currency: "00")
+tkn = CommunityCustomization.create(community_id: 1, name: "The Kitchen Network", locale: "en", slogan: "Rent a Kitchen Near You", description: "This is your destination for renting commercial kitchens, experienced foodservice professionals, and food business consulting aid. ")
+
+### Add some users
+admin = Person.create(community_id: 1, is_admin:true, locale: "en", username: "superuser", password: "testtest123")
+
+user1 = Person.create(community_id: 1, is_admin:false, locale: "en", username: "defaultuser1", password: "testuser1test")
+
+user2 = Person.create(community_id: 1, is_admin:false, locale: "en", username: "defaultuser2", password: "testuser2test")
+
+user3 = Person.create(community_id: 1, is_admin:false, locale: "en", username: "defaultuser3", password: "testuser3test")
+
+### Add users to TKN
+adminMember = CommunityMembership.create(person_id: admin.id, community_id: community.id, consent: "accepted")
+member = CommunityMembership.create(person_id: user1.id, community_id: community.id, consent: "accepted")
+member2 = CommunityMembership.create(person_id: user2.id, community_id: community.id, consent: "accepted")
+member3 = CommunityMembership.create(person_id: user3.id, community_id: community.id, consent: "accepted")
